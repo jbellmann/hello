be940342088dceb93c634712fb9b5a63452dabe9
Joerg Bellmann <joerg.bellmann@googlemail.com>
2018-01-06 11:23:07 +0100

    add docker build

--- src/test/java/de/jbellmann/hello/HelloApplicationTests.java
+++ src/test/java/de/jbellmann/hello/HelloApplicationTests.java
@@ -1,16 +1,33 @@
 package de.jbellmann.hello;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.LocalManagementPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class HelloApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @LocalManagementPort
+    private int managementPort;
+
+    @Autowired
+    private TestRestTemplate rest;
+
+    @Test
+    public void contextLoads() {
+        ResponseEntity<String> response = rest.getForEntity("http://localhost:" + managementPort + "/management/health",
+                String.class);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 
 }
